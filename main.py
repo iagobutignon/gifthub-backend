33b3395615121475ada3d75a0a5d62969c94e7db
Iago <iagobutignon.fatec@gmail.com>
2023-12-03 16:59:21 -0300

    CORS

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from app.modules.user.user_controller import user_blueprint
 from app.modules.event.event_controller import event_blueprint
 from app.modules.product.product_controller import product_blueprint
 from app.modules.address.address_controller import address_blueprint
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 configure_database(app)
+CORS(app, resources={r"/": {"origins": ""}}, supports_credentials=True)
 
 app.register_blueprint(user_blueprint, url_prefix="/user")
 app.register_blueprint(event_blueprint, url_prefix="/event")
